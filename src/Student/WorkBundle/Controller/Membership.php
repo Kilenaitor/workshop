5932b99c2835583b51dbb9b7d461e92ac462f2cc
Kyle Minshall <kyleminshall@gmail.com>
2014-10-25 22:50:06 -0700

    Removing code to be filled in at workshop

--- src/Student/WorkBundle/Controller/Membership.php
+++ src/Student/WorkBundle/Controller/Membership.php
@@ -8,83 +8,22 @@ class Membership
 {
     static function confirm($session)
     {
-        if(!isset($session))
-        {
-            return false;
-        }
-		else if($session->get('status', 'no') != 'authorized') //If they are not authorized in the session
-		{
-            return false;
-		}
-        return true;
+        /* Code goes here */
 	}
     
     static function validateUser($username, $password, $session = null)
 	{
-		$password = hash('sha256', $password); //Hash the input password using SHA256 encryption algorithm (No salt because I'm lame)
-		$ensure_credentials = self::verify($username, $password); //Call the verify method in the mysql class
-	
-		if(!is_null($ensure_credentials)) //No errors means successful authentication
-		{
-			$username = self::getUsername($username);
-			$session->set('status', 'authorized'); //Set their session status to authorized
-			$session->set('username', $username); //Set their session username to their username for global access throughout the site
-            return array('status' => 'success');
-		} 
-		else 
-			return array('status' => 'failure'); //Otherwise return an error that they had an incorrect username or password
+		/* Code goes here */
 	}
     
     static function verify($username, $password)
 	{
-		$con = mysqli_connect("localhost","root","","Student"); //Connect to the database
-	
-		if(mysqli_connect_errno()) 
-		{
-		  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-          die;
-		}
-	
-		$query = "SELECT * FROM users WHERE username='$username'"; //Lookup username
-	
-		$result = mysqli_query($con, $query) or trigger_error(mysqli_error()." ".$query);
-	
-		$row = mysqli_fetch_assoc($result);
-	
-		if($password === $row['password']) //Check to see if their entered password matches the one from their table entry
-		{
-			mysqli_close($con);
-			return $row['username']; //If it matches, they're in!
-		}
-		else
-		{
-			mysqli_close($con);
-			return; //Otherwise, kick them out
-		}
+        /* Code goes here */
 	}
     
     static function signup($username, $password, $session)
     {
-        $password = hash('sha256', $password); //Hash the input password using SHA256 encryption algorithm (No salt because I'm lame)
-        $con = mysqli_connect("localhost","root","","Student"); //Connect to the database
-		if(mysqli_connect_errno()) 
-		{
-		  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-          die;
-		}
-        $query = mysqli_query($con, "SELECT * FROM users WHERE username='$username'"); //Lookup username
-        $row = mysqli_fetch_assoc($query);
-        if(!empty($row))
-        {
-            return array('status' => 'failure', 'message' => 'Username is already taken');
-        }
-        
-        $query = "INSERT INTO users (`username`, `password`) VALUES ('$username', '$password')";
-        mysqli_query($con, $query);
-		$username = self::getUsername($username);
-		$session->set('status', 'authorized'); //Set their session status to authorized
-		$session->set('username', $username); //Set their session username to their username for global access throughout the site
-        return array('status' => 'success');
+        /* Code goes here */
     }
     
     static function getUsername($username)
--- src/Student/WorkBundle/Controller/WorkController.php
+++ src/Student/WorkBundle/Controller/WorkController.php
@@ -8,87 +8,26 @@ class WorkController extends Controller
 {
     public function indexAction()
     {
-        $session = $this->getRequest()->getSession();
-        if(!Membership::confirm($session))
-            return $this->redirect($this->generateUrl('login'));
-        
-        $session = $this->getRequest()->getSession();
-
-        $con=mysql_connect("localhost","root",""); //Start a connection to the database. :)
-        $db_selected = mysql_select_db('Student', $con); //Select the "Site" database
-
-        $username = $session->get('username');
-        $name = mysql_result(mysql_query("SELECT username FROM users WHERE username='$username'"), 0);
-        
+		/* Code goes here */
+		$name = "name";
         return $this->render('StudentWorkBundle:Work:index.html.twig', array('name' => $name));
     }
     
     public function loginAction()
     {
-        $session = $this->getRequest()->getSession();
-		
-		if($_POST && empty($_POST['username']))
-		{
-			$error = "Username cannot be blank";
-            return $this->render('StudentWorkBundle:Work:login.html.twig', array('error' => $error));
-		}
-		
-		if($_POST && empty($_POST['password']))
-		{
-			$error = "Password cannot be blank";
-            return $this->render('StudentWorkBundle:Work:login.html.twig', array('error' => $error));
-		}
-        
-        if($_POST && !empty($_POST['username']) && !empty($_POST['password']))
-		{
-            $response = Membership::validateUser($_POST['username'], $_POST['password'], $session);	//Validate the user when they click submit on the login
-        
-	        if($response['status'] == 'success')
-	            return $this->redirect($this->generateUrl('home'));
-		    else if($response['status'] == 'failure')
-			{
-				$error = "Incorrect Username or Password";
-	            return $this->render('StudentWorkBundle:Work:login.html.twig', array('error' => $error));
-			}
-		}
+     	/* Code goes here */
 		return $this->render('StudentWorkBundle:Work:login.html.twig');
     }
     
     public function logoutAction()
     {
-        $session = $this->getRequest()->getSession();
-        $session->clear();
+       	/* Code goes here */
         return $this->redirect($this->generateUrl('home'));
     }
 	
 	public function signupAction()
 	{
-		$session = $this->getRequest()->getSession();
-		
-		if($_POST && empty($_POST['username']))
-		{
-			$error = "Username cannot be blank";
-            return $this->render('StudentWorkBundle:Work:signup.html.twig', array('error' => $error));
-		}
-		
-		if($_POST && empty($_POST['password']))
-		{
-			$error = "Password cannot be blank";
-            return $this->render('StudentWorkBundle:Work:signup.html.twig', array('error' => $error));
-		}
-        
-        if($_POST && !empty($_POST['username']) && !empty($_POST['password']))
-		{
-            $response = Membership::signup($_POST['username'], $_POST['password'], $session);	//Validate the user when they click submit on the login
-        
-	        if($response['status'] == 'success')
-	            return $this->redirect($this->generateUrl('home'));
-		    else if($response['status'] == 'failure')
-			{
-				$error = $response['message'];
-	            return $this->render('StudentWorkBundle:Work:signup.html.twig', array('error' => $error));
-			}
-		}
+		/* Code goes here */
 		return $this->render('StudentWorkBundle:Work:signup.html.twig');
 	}
 }
