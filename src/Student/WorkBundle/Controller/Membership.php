6951e4bea235eb536152852f3b9465375d07dd67
Kyle Minshall <kyleminshall@gmail.com>
2014-11-08 23:53:39 -0800

    Updating code! Simplifying some things

--- src/Student/WorkBundle/Controller/Membership.php
+++ src/Student/WorkBundle/Controller/Membership.php
@@ -8,91 +8,68 @@ class Membership
 {
     static function confirm($session)
     {
-        if(!isset($session))
-        {
+        if($session->get('status', 'no') != 'authorized')
             return false;
-        }
-		else if($session->get('status', 'no') != 'authorized') //If they are not authorized in the session
-		{
-            return false;
-		}
         return true;
 	}
     
     static function validateUser($username, $password, $session = null)
 	{
-		$password = hash('sha256', $password); //Hash the input password using SHA256 encryption algorithm (No salt because I'm lame)
-		$ensure_credentials = self::verify($username, $password); //Call the verify method in the mysql class
+		$password = hash('sha256', $password);
+        
+        $con = self::connectToDatabase();
+        
+		$query = "SELECT * FROM users WHERE username='$username'"; //Lookup username
+		$result = mysqli_query($con, $query) or trigger_error(mysqli_error()." ".$query);
+		$row = mysqli_fetch_assoc($result);
+        
+        $verified = false;
 	
-		if(!is_null($ensure_credentials)) //No errors means successful authentication
+		if($password === $row['password'])
+			$verified = true;
+	
+		if($verified)
 		{
 			$username = self::getUsername($username);
-			$session->set('status', 'authorized'); //Set their session status to authorized
-			$session->set('username', $username); //Set their session username to their username for global access throughout the site
+			$session->set('status', 'authorized');
+			$session->set('username', $username);
             return array('status' => 'success');
 		} 
 		else 
-			return array('status' => 'failure'); //Otherwise return an error that they had an incorrect username or password
-	}
-    
-    static function verify($username, $password)
-	{
-		$con = mysqli_connect("localhost","root","","Student"); //Connect to the database
-	
-		if(mysqli_connect_errno()) 
-		{
-		  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-          die;
-		}
-	
-		$query = "SELECT * FROM users WHERE username='$username'"; //Lookup username
-	
-		$result = mysqli_query($con, $query) or trigger_error(mysqli_error()." ".$query);
-	
-		$row = mysqli_fetch_assoc($result);
-	
-		if($password === $row['password']) //Check to see if their entered password matches the one from their table entry
-		{
-			mysqli_close($con);
-			return $row['username']; //If it matches, they're in!
-		}
-		else
-		{
-			mysqli_close($con);
-			return; //Otherwise, kick them out
-		}
+			return array('status' => 'failure');
 	}
     
     static function signup($username, $password, $session)
     {
-        $password = hash('sha256', $password); //Hash the input password using SHA256 encryption algorithm (No salt because I'm lame)
-        $con = mysqli_connect("localhost","root","","Student"); //Connect to the database
-		if(mysqli_connect_errno()) 
-		{
-		  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-          die;
-		}
-        $query = mysqli_query($con, "SELECT * FROM users WHERE username='$username'"); //Lookup username
+        $password = hash('sha256', $password);
+        
+        $con = self::connectToDatabase();
+        $query = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
         $row = mysqli_fetch_assoc($query);
         if(!empty($row))
-        {
             return array('status' => 'failure', 'message' => 'Username is already taken');
-        }
         
         $query = "INSERT INTO users (`username`, `password`) VALUES ('$username', '$password')";
         mysqli_query($con, $query);
-		$username = self::getUsername($username);
-		$session->set('status', 'authorized'); //Set their session status to authorized
-		$session->set('username', $username); //Set their session username to their username for global access throughout the site
+		
+        $username = self::getUsername($username);
+		$session->set('status', 'authorized'); 
+		$session->set('username', $username);
+        
         return array('status' => 'success');
     }
     
     static function getUsername($username)
 	{
-		$con=mysqli_connect("localhost","root","", "Student"); //Connect to the database
-        
+		$con = self::connectToDatabase();
         $query = mysqli_query($con, "SELECT username FROM users WHERE username='$username'");
+        
         return $query->fetch_assoc()['username'];
 	}
+    
+    static function connectToDatabase()
+    {
+        return mysqli_connect("localhost","root","","Student");
+    }
     
 }
--- src/Student/WorkBundle/Controller/WorkController.php
+++ src/Student/WorkBundle/Controller/WorkController.php
@@ -11,14 +11,9 @@ class WorkController extends Controller
         $session = $this->getRequest()->getSession();
         if(!Membership::confirm($session))
             return $this->redirect($this->generateUrl('login'));
-        
-        $session = $this->getRequest()->getSession();
-
-        $con=mysql_connect("localhost","root",""); //Start a connection to the database. :)
-        $db_selected = mysql_select_db('Student', $con); //Select the "Site" database
 
         $username = $session->get('username');
-        $name = mysql_result(mysql_query("SELECT username FROM users WHERE username='$username'"), 0);
+        $name = Membership::getUsername($username);
         
         return $this->render('StudentWorkBundle:Work:index.html.twig', array('name' => $name));
     }
@@ -41,8 +36,7 @@ class WorkController extends Controller
         
         if($_POST && !empty($_POST['username']) && !empty($_POST['password']))
 		{
-            $response = Membership::validateUser($_POST['username'], $_POST['password'], $session);	//Validate the user when they click submit on the login
-        
+            $response = Membership::validateUser($_POST['username'], $_POST['password'], $session);	//Validate the user when they click submit
 	        if($response['status'] == 'success')
 	            return $this->redirect($this->generateUrl('home'));
 		    else if($response['status'] == 'failure')
